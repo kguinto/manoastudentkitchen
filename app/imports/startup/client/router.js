9fb1130351ad7b995ec098645765fb84d6db2f37
Curtis Frifeldt <cfrifel@hawaii.edu>
2017-04-13 07:12:14 -1000

    finished editUserMockup

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -60,6 +60,14 @@ userRoutes.route('/profile', {
   },
 });
 
+export const editProfilePageRouteName = 'Edit_Profile_Page';
+userRoutes.route('/edit-profile', {
+  name: editProfilePageRouteName,
+  action() {
+    BlazeLayout.render('User_Layout', { main: editProfilePageRouteName });
+  },
+});
+
 export const filterPageRouteName = 'Filter_Page';
 userRoutes.route('/filter', {
   name: filterPageRouteName,
