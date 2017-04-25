a9862eb0a28dab12e48a05266df34570c203564f
Alex Watanabe <alexcw@hawaii.edu>
2017-04-25 11:40:46 -1000

    Fix errors, replace database initialization file and add sample tags data to test

--- app/imports/startup/server/publications.js
+++ app/imports/startup/server/publications.js
@@ -1,5 +1,8 @@
 import { Interests } from '/imports/api/interest/InterestCollection';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Tags } from '/imports/api/tag/TagCollection';
 
 Interests.publish();
 Profiles.publish();
+Tags.publish();
+
--- app/imports/ui/pages/home/home-page.js
+++ app/imports/ui/pages/home/home-page.js
@@ -3,6 +3,11 @@ import { Recipes } from '/imports/api/profile/ProfileCollection';
 import { Tags } from '/imports/api/tag/TagCollection';
 import { _ } from 'meteor/underscore';
 
+
+Template.Home_Page.onCreated(function onCreated() {
+  this.subscribe(Tags.getPublicationName());
+});
+
 Template.Home_Page.helpers({
 
   /**
@@ -18,10 +23,12 @@ Template.Home_Page.helpers({
    *
    */
   top_tags() {
+    console.log(Tags.find({}, {}));
     const allTags = Tags.find({}, { fields: { tagName: 1 } }).fetch();
     const namesOnly = _.values(allTags);
     const frequency = _.countBy(namesOnly, function (each) { return each; });
-    const result = _.first(_.sortBy(_.uniq(namesOnly), function (frequencyKey) { return -frequency[frequencyKey]; }), 8);
+    const result = _.first(_.sortBy(_.uniq(namesOnly),
+        function (frequencyKey) { return -frequency[frequencyKey]; }), 8);
     return result;
   },
 
