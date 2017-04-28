ed9aaf43aebf9f2152d87d93906b23580801eda4
kguinto <klogold1@gmail.com>
2017-04-27 15:05:10 -1000

    Recipe page can now read from database.

--- app/imports/ui/layouts/recipe/index.js
+++ app/imports/ui/layouts/recipe/index.js
@@ -2,4 +2,4 @@ import './recipe-layout.html';
 import './recipe-header.html'
 import './recipe-header';
 import './recipe-footer.html';
-import './recipe-layout.html';
+import './recipe-layout.css';
