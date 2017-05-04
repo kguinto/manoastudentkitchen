0d043f5b558d211dc773ce16f03d602bdf1f4ce9
Alex Watanabe <alexcw@hawaii.edu>
2017-05-04 12:42:34 -1000

    Improve search to include tags when comma delimited.

--- app/imports/ui/pages/search/search-page.js
+++ app/imports/ui/pages/search/search-page.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { Recipes } from '/imports/api/recipe/RecipeCollection';
 import { Tags } from '/imports/api/tag/TagCollection';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { _ } from 'meteor/underscore';
 
 Template.View_Search_Page.onCreated(function onCreated() {
   this.subscribe(Tags.getPublicationName());
@@ -16,7 +17,19 @@ Template.View_Search_Page.helpers({
    */
   search_results() {
     const param = FlowRouter.getParam('searchParam');
-    return Recipes.find({ recipeName: { $regex: `${param}` } }, { sort: { viewcount: -1, limit: 16 } }).fetch();
+
+    const results = Recipes.find({ recipeName: { $regex: `${param}` } }, { sort: { viewcount: -1 } }).fetch();
+
+    let tagSearchArr = param.split(',');
+    tagSearchArr = _.map(tagSearchArr, function snip(term) { return term[0] === ' ' ? term.substr(1) : term; });
+    const tagSearchMap = _.map(tagSearchArr, function searchterm(term) { return { tagName: term }; });
+    const tagSearchResults = Tags.find({ $or: tagSearchMap }, { fields: { recipeID: 1 } }).fetch();
+    const tagSearchResultsRenamed = _.map(tagSearchResults, function rename(item) { return { _id: item.recipeID }; });
+    if (tagSearchResultsRenamed.length > 0) {
+      const tagSearch = Recipes.find({ $or: tagSearchResultsRenamed }, { sort: { viewcount: -1 } }).fetch();
+      results.push(tagSearch);
+    }
+    return results;
   },
 
   /**
