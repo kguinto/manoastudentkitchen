a3dfb600c9b906db9c99754103520a32e0ff1e4c
kguinto <klogold1@gmail.com>
2017-05-07 14:17:44 -1000

    Page fixes, home page tag links

--- app/imports/ui/pages/search/search-page.js
+++ app/imports/ui/pages/search/search-page.js
@@ -21,7 +21,7 @@ Template.View_Search_Page.helpers({
   search_results() {
     const param = FlowRouter.getParam('searchParam');
 
-    const results = Recipes.find({ recipeName: { $regex: `${param}` } }, { sort: { viewcount: -1 } }).fetch();
+    const results = Recipes.find({ recipeName: { $regex: '${param}' } }, { sort: { viewcount: -1 } }).fetch();
 
     let tagSearchArr = param.split(',');
     tagSearchArr = _.map(tagSearchArr, function snip(term) { return term[0] === ' ' ? term.substr(1) : term; });
@@ -32,7 +32,8 @@ Template.View_Search_Page.helpers({
       const tagSearch = Recipes.find({ $or: tagSearchResultsRenamed }, { sort: { viewcount: -1 } }).fetch();
       results.push(tagSearch);
     }
-    return results;
+    console.log(results[0]);
+    return results[0];
   },
 
   /**
@@ -51,5 +52,40 @@ Template.View_Search_Page.helpers({
 
   get_recipe_url(recipeID) {
     return `/recipe/${recipeID}/view`;
+  },
+
+  search_param(){
+    return FlowRouter.getParam('searchParam');
+  },
+
+  search_title(){
+    if (search_results().length == 1)
+      return search_results().length + ' result for \'' + search_param() + '\'';
+    else if (search_results().length > 1)
+      return search_results().length + ' results for \'' + search_param() + '\'';
+    else
+      return '0 results for \'' + search_param() + '\'';
   }
 });
+
+function search_param(){
+  return FlowRouter.getParam('searchParam');
+}
+
+function search_results(){
+  const param = FlowRouter.getParam('searchParam');
+
+  const results = Recipes.find({ recipeName: { $regex: `${param}` } }, { sort: { viewcount: -1 } }).fetch();
+
+  let tagSearchArr = param.split(',');
+  tagSearchArr = _.map(tagSearchArr, function snip(term) { return term[0] === ' ' ? term.substr(1) : term; });
+  const tagSearchMap = _.map(tagSearchArr, function searchterm(term) { return { tagName: term }; });
+  const tagSearchResults = Tags.find({ $or: tagSearchMap }, { fields: { recipeID: 1 } }).fetch();
+  const tagSearchResultsRenamed = _.map(tagSearchResults, function rename(item) { return { _id: item.recipeID }; });
+  if (tagSearchResultsRenamed.length > 0) {
+    const tagSearch = Recipes.find({ $or: tagSearchResultsRenamed }, { sort: { viewcount: -1 } }).fetch();
+    results.push(tagSearch);
+  }
+  console.log(results[0]);
+  return results[0];
+}
